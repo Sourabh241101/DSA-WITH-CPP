bce4b9cc3a36ef52643f91b3b3794ba53cea9adf
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2024-09-26 23:41:34 +0530

    Add files via upload

---
+++ 2-D Array/01Declaration.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[3][3];  //  declaration
+	// row -> 3 ( 0 1 2)
+	//column -> 3 (0 1 2)
+	 int brr[][3]={{1,2,3},{4,5,6},{7,8,9}} ;
+	arr[0][0]=4;
+	cout<<arr[0][0]<<endl;
+	cout<<brr[2][1];
+	
+	
+}
