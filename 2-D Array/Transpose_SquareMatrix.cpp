cbe1c3605466259e8a85e20158b2206258f90b76
Sourabh Gurwani <118509627+SourabhGurwani@users.noreply.github.com>
2024-09-29 22:09:29 +0530

    Add files via upload

---
+++ 2-D Array/Transpose_SquareMatrix.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int m;
+	cout<<"Enter the no of rows/cols: ";
+	cin>>m;
+	
+	int arr[m][m];
+	
+	//transpose in the same matrix
+	
+	for(int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+			cin>>arr[i][j];
+		}
+	}
+	  cout<<"Matrix before transpose: "<<endl;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+			cout<<arr[i][j]<<" ";
+		} 
+		cout<<endl;
+	}
+	
+	//transpose in the same matrix
+	for(int i=0;i<m;i++){
+		for(int j=i+1;j<m;j++){
+			   int temp=arr[i][j];
+			   arr[i][j]=arr[j][i];
+			   arr[j][i]=temp;
+		}
+	}
+	cout<<"Matrix after transpose: "<<endl;
+	for(int i=0;i<m;i++){
+		for(int j=0;j<m;j++){
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	
+	
+	
+	
+	
+}
